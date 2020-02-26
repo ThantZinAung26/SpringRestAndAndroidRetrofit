dde319a633c1e497ec37668292ecad0221282919
Thant Zin Aung <thantzinblogger@gmail.com>
2020-02-26 09:07:16 +0630

    file service

--- SpringRestApi/src/main/kotlin/thant/soft/thantsinaung/model/Product.kt
+++ SpringRestApi/src/main/kotlin/thant/soft/thantsinaung/model/Product.kt
@@ -1,20 +1,51 @@
 package thant.soft.thantsinaung.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 data class Product(
         var name: String,
         var description: String,
+        var fileName: String,
+        @Lob
+        var file: ByteArray,
         var available: Boolean) {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Long = 0L
 
-        constructor() : this("", "",false)
+        /*var fileName: String = "test"
+
+        @Lob
+        var file: ByteArray = "Photo".toByteArray()*/
+
+        constructor() : this("", "", "", "test".toByteArray(charset = Charsets.UTF_8),false)
+
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as Product
+
+                if (name != other.name) return false
+                if (description != other.description) return false
+                if (fileName != other.fileName) return false
+                if (!file.contentEquals(other.file)) return false
+                if (available != other.available) return false
+                if (id != other.id) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                var result = name.hashCode()
+                result = 31 * result + description.hashCode()
+                result = 31 * result + fileName.hashCode()
+                result = 31 * result + file.contentHashCode()
+                result = 31 * result + available.hashCode()
+                result = 31 * result + id.hashCode()
+                return result
+        }
 
 }
---
+++ SpringRestApi/src/main/kotlin/thant/soft/thantsinaung/service/FileService.kt
@@ -0,0 +1,10 @@
+package thant.soft.thantsinaung.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class FileService {
+
+
+
+}
