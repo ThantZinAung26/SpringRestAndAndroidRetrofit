2aa793898ba3efb8faf022c358e454964d084955
thantsinaung <thantsinaung@op-j.jp>
2020-02-26 16:16:40 +0630

    init

---
+++ SpringbootFileUploadTest/src/main/kotlin/com/ceo26/sfu/SpringbootFileUploadTestApplication.kt
@@ -0,0 +1,11 @@
+package com.ceo26.sfu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+	class SpringbootFileUploadTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootFileUploadTestApplication>(*args)
+}
